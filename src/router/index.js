39c78f31ded1ef5d938fea83a5cb74146c3109e0
Fakhri Abdillah <fakhriabdillah752@gmail.com>
2025-11-06 09:10:01 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from "vue-router";
+ 
+import dashboard from '../pages/master/dashboard.vue'
+
+import home from '../pages/home.vue'
+import profile from '../pages/profile.vue'
+
+  const routes = [
+    {
+      name: 'Dashboard',
+      path: '/',
+      component: dashboard,
+      children: [
+         {
+          name: 'home',
+          path: '/home',
+          component:home
+        },
+        {
+          name: 'profile',
+          path: '/profile',
+          component:profile
+        }
+      ]
+    },
+       
+  ];
+const router = Router();
+export default router;
+function Router() {
+    const router = new createRouter({
+        history: createWebHistory(),
+        routes,
+    });
+    return router;
+}
+  
